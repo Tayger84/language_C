808de73bc3c8d4cb009c9404f66bd69a2232dfe3
Josef Ďuriš <josef.duris84@gmail.com>
2025-12-17 14:59:57 +0100

    Add files via upload

---
+++ kru_io.c
@@ -0,0 +1,54 @@
+/*
+* kru_io.c ve. 2.0
+*
+* Vstupy a vystupy programu pro vypocty kruznice 
+* ==============================================
+*
+* P. Herout, rijen 2003
+*
+*/
+
+/* systemove hlavickove soubory */
+#include <stdio.h>
+
+/* vlastni hlavickove soubory */
+#include "kru_io.h" /* natazeni symbolickych konstant, funkcnich prototypu globalnich funkci a globalnich typu vlastnich modulu */
+#include "kru_main.h" /* natazeni symbolickych konstant, funkcnich prototypu globalnich funcki a globalnich typu spolupracujiciho modulu */
+
+/* definice globalnich promennych */
+ /* v tomto modulu zadne nejsou */
+
+/* lokalni definice symbolickych konstat a maker */
+#define kontrola(x)  ( ((x) >= 0.0) ?  (x) : CHYBA_DAT )
+
+/* lokalni definice novych typu */
+ /* v tomto modulu zadne nejsou */
+
+/* definice statickych globalnich promennych */
+static double polomer;
+
+/* uplne funkcni prototypy lokalnich funkci */
+ /* v tomto modulu zadne nejsou */
+
+/* funkce main() */
+ /* v tomto modulu neni */
+
+/* definice lokalnich funkci */
+ /* v tomto modulu zadne nejsou */
+
+/* definice globalnich funkci */
+double vstup_dat(void) {
+    printf("\nZadej polomer kruznice (kladne realne cislo): ");
+    scanf("%lf", &polomer);
+
+    return (kontrola(polomer));
+}
+
+void vystup_dat(double obsah) {
+    double prumer;
+
+    printf("Obsah kruhu o polomeru %6.2f je %.2f\n", polomer, obsah);
+
+    prumer = vyp_prumer(polomer);
+    printf("Obvod kruhu o prumeru %6.2f je %.2f\n", prumer, obvod);
+}
---
+++ kru_main.h
@@ -0,0 +1,31 @@
+/*
+* kru_main.h ver. 2.0
+*
+* Hlavickovy soubor pro modul kru_main.c
+* ======================================
+*
+* P. Herout, rijen 2003
+*
+*/
+
+/* podmineny preklad proti opakovanemu inkludovani */
+#ifndef KRU_MAIN_H
+#define KRU_MAIN_H
+
+/* definice symbolických konstant vyuzivanych i v jinych modulech */
+ /* v tomto modulu zadne nejsou */
+
+
+/* definice maker s parametry */
+ /* v tomto modulu zadne nejsou */
+
+/* definice globalnich typu */
+ /* v tomto modulu zadne nejsou */
+
+ /* deklarace golbalnich promennych modulu kru_main.c */
+ extern double obvod;
+
+ /* uplne funkcni prototypy globalnich funkci modulu kru_main.c */
+ extern double vyp_prumer(double polomer);
+
+ #endif
